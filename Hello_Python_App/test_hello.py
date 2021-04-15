62c058a9cc2ab0b88567e6926c67d35e710c377f
NP <np@workmail.com>
2021-04-15 07:18:34 -0400

    Add tests for all functions

--- Hello_Python_App/test_hello.py
+++ Hello_Python_App/test_hello.py
@@ -8,7 +8,13 @@ def teardown_function(function):
     print("Running Teardown: %s" % function.__name__)
     del function.x
 
-### Test 1 function
+### Test all functions in hello.py
+
+def test_hello_add():
+   assert add(test_hello_add.x) == 11
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
 
 def test_toyou():
     assert toyou(test_toyou.x) == "hi 10"

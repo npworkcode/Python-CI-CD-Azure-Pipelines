c8c416e94450b3852f88b6bd24c8749094cb38fa
NP <np@workmail.com>
2021-04-17 18:00:38 -0400

    Added app name to script

--- Flask_ML_Python_App-Azure_Pipelines/make_predict_azure_app.sh
+++ Flask_ML_Python_App-Azure_Pipelines/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://python04152021-ml-project2.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
